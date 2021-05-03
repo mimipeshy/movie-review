c91e0a4fc8fbd097a75e7ff0750e026f6622f53b
Mimi Peshy <perisndanu@gmail.com>
2021-05-03 21:48:35 +0300

    Display product components in App

--- src/containers/ProductComponent.js
+++ src/containers/ProductComponent.js
@@ -1,26 +1,36 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
 const ProductComponent = () => {
   const products = useSelector((state) => state.allProducts.products);
-  // const { title } = products;
-  return (
-    <div className="four comumn wide">
-      <div className="ui link cards">
-        <div className="card">
-          <div className="image">
-            Image
+  const renderList = products.map((product) => {
+    const {
+      id, title, image, price, category,
+    } = product;
+    return (
+      <div className="four wide column" key={id}>
+        <Link to={`/product/${id}`}>
+          <div className="ui link cards">
+            <div className="card">
+              <div className="image">
+                <img src={image} alt={title} />
+              </div>
+              <div className="content">
+                <div className="header">{title}</div>
+                <div className="meta price">
+                  $
+                  {price}
+                </div>
+                <div className="meta">{category}</div>
+              </div>
+            </div>
           </div>
-          <div className="content">
-            content
-          </div>
-          {/* <div className="header">
-            {title}
-          </div> */}
-        </div>
+        </Link>
       </div>
-      <h4>Product Component</h4>
-    </div>
-  );
+    );
+  });
+  return <>{renderList}</>;
 };
+
 export default ProductComponent;
