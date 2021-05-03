8da2ca1da524ce2918a7c1cde024d0ffca05bdcb
Mimi Peshy <perisndanu@gmail.com>
2021-05-03 21:16:28 +0300

    Fetch product data from API

--- src/containers/ProductListing.js
+++ src/containers/ProductListing.js
@@ -11,7 +11,7 @@ const ProductListing = () => {
       .catch((err) => {
         console.log('Err', err);
       });
-    console.log(response.data);
+    console.log(response);
   };
   useEffect(() => {
     fetchProducts();
