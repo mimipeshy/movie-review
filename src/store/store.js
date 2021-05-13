d81bbcfdf705b5a40a8718b2e6b06fa50a7254c8
Mimi Peshy <perisndanu@gmail.com>
2021-05-13 23:08:36 +0300

    Fix reducer route error

--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,7 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
-import rootReducer from './reducers';
+import rootReducer from '../reducers/index';
 
 const middleware = [thunk];
 const initialState = {};
